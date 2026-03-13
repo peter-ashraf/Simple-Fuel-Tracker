307d970eb88057fddf6a1f2da0e1dd79e275b54e
peter-ashraf <peter.ashraf16@gmail.com>
2026-03-13 11:08:57 +0200

    fix: switch to HashRouter for stable GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { FuelProvider } from './hooks/useFuelContext.jsx'
 import { ThemeProvider } from './hooks/useTheme.jsx'
 import './index.css'
@@ -10,9 +10,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <FuelProvider>
-        <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, '') || '/'}>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </FuelProvider>
     </ThemeProvider>
   </StrictMode>,
